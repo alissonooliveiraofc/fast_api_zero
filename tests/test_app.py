1b2d6d3b61271fb5bac42bc7af645fbc363e98fa
Alisson Oliveira <alissonooliveiraofc@gmail.com>
2025-11-01 17:38:29 -0300

    adiciona endpoint para deletar usuário e tratamento de erro para usuário não encontrado

--- tests/test_app.py
+++ tests/test_app.py
@@ -57,3 +57,14 @@ def test_update_user(client):
         "username": "sheldon cooper",
         "email": "sheldon@bazinga.com",
     }
+
+
+def test_delete_user(client):
+    response = client.delete("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "id": 1,
+        "username": "sheldon cooper",
+        "email": "sheldon@bazinga.com",
+    }
